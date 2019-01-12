5838a29fdf3f79b4a3d82a3e9e8d6d1ddd9e2af3
Borui Tao <borui.tao@mail.mcgill.ca>
2019-01-12 17:59:32 -0500

    add camera movement

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstScene", 2f);
+	}
+	
+	// Update is called once per frame
+	void LoadFirstScene () {
+        SceneManager.LoadScene(1);
+	}
+}
